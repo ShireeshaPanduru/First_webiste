8b50d1b6b3e47d9311e497415ed9f1a484450ebf
ShireeshaPanduru <121652807+ShireeshaPanduru@users.noreply.github.com>
2023-03-26 11:29:28 +0530

    Add files via upload

---
+++ javascript/jq.js
@@ -0,0 +1,466 @@
+// $('p').text('hai..');
+
+// $('p').click(function(){$(this).text('hai..');});
+
+
+// $('#dd').click(function(){alert("u clicked on dd");});
+
+// $('.altek').click(function(){alert("u clicked on altek");});
+
+
+
+// $('#p2').text('Username');
+
+// $('#p1').click(function(){alert("u clicked on fibernet");});
+
+// $('#main_section2').click(function(){alert("u clicked on Enquire now");});
+// $('#for').click(function(){alert("u clicked on Forgot password");});
+
+// $('#left4').click(function(){alert("u choosen clear button");});
+
+// $('#left3').click(function(){alert("u choosen submit the details");});
+
+// $('#readd').click(function(){alert("you have choosen Read more option");});
+
+// $('#left33').click(function(){alert("you clicked on sign in");});
+// $('#left44').click(function(){alert("u choosen clear button");});
+
+
+// var a=$('#lst').find('*').length;alert(a);
+
+// $(document).ready(function(){$('#para').fadeIn(1000);});
+
+// $(document).ready(function(){
+	// $('h3').hide();
+	// $('h2').click(function(){$('h3').show(3000);
+// });
+
+// });
+
+// $(document).ready(function(){
+	// $('p').hide();
+	// $('h1').click(function(){
+		// $(this).next().slideToggle(1000);
+	// });
+	// $('h2').click(function(){
+		// $(this).next().slideToggle(1000);
+	// });
+	// $('h3').click(function(){
+		// $(this).next().slideToggle(1000);
+	// });
+// });
+
+// $('p').text('hai..');
+// --------------------------------------
+
+
+$(document).ready(function(){
+	$('#scc').hide();
+	$('#p1').click(function(){
+		$(this).next().slideToggle(4000);
+	});
+	
+});
+
+
+
+
+$(document).ready(function(){
+	$('#lead').hide();
+	$('#img16').click(function(){
+		$(this).next().slideToggle(2000);
+	});
+	
+});
+
+$(document).ready(function(){
+	$('#dd9').hide();
+	$('#vie').click(function(){
+		$(this).next().slideToggle(2000);
+	});
+	
+});
+
+$(document).ready(function(){
+	$('#homm').hide();
+	$('#img1').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+
+$(document).ready(function(){
+	$('#invest').hide();
+	$('#inv').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+$(document).ready(function(){
+	$('#turn').hide();
+	$('#high').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+$(document).ready(function(){
+	$('#sup').hide();
+	$('#pro').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+$(document).ready(function(){
+	$('#train').hide();
+	$('#trai').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+$(document).ready(function(){
+	$('#supp').hide();
+	$('#market').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+
+$(document).ready(function(){
+	$('#quality').hide();
+	$('#quali').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+
+$(document).ready(function(){
+	$('#para').hide();
+	$('#get').click(function(){
+		$(this).next().slideToggle(1000);
+	});
+	
+});
+$(document).ready(function(){
+	$('#child').hide();
+	$('#test').click(function(){
+		$(this).next().slideToggle(6000);
+	});
+	
+});
+
+$(document).ready(function(){
+	
+	
+	$('#invest').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	$('#turn').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	$('#sup').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	$('#train').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	$('#supp').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	$('#quality').mouseenter(function(){
+	$(this).css('background','#50D890')
+	});
+	
+	
+	
+});
+//slide toggle
+// $('#clk').click(function(){$('#image').slideToggle(2000)});
+
+// $('document').ready(function(){
+	// $('#h1').mousedown(function(){
+	// $(this).css('background','red')
+	// });
+	
+	// $('#h1').mouseup(function(){
+	// $(this).css('background','orange')
+	// });
+	
+	// $('#h1').mouseenter(function(){
+	// $(this).css('background','skyblue')
+	// });
+	
+	// $('#h1').mouseleave(function(){
+	// $(this).css('background','green')
+	// });
+// });
+
+
+	
+
+// practice	
+
+
+// 1.addClass()
+// $( "p" ).last().addClass().css('color','green');
+	
+	// });
+	
+	
+// $( "#result1" ).append( $( "p" ).first().hasClass( "selected" ).toString().css('color','red') );
+
+
+
+// $('document').ready(function(){
+	
+// $('#btn1').click(function(){
+	// $('#img1').hide(2000);
+	// })
+// $('#btn2').click(function(){
+	// $('#img1').show(2000);
+	// })
+// $('#btn3').click(function(){
+	// $('#img1').toggle(3000);
+	// })
+// $('#btn4').click(function(){
+	// $('#img1').fadeIn(2000);
+	// })
+// $('#btn5').click(function(){
+	// $('#img1').fadeOut(2000);
+	// })	
+// $('#btn6').click(function(){
+	// $('#img1').fadeToggle(2000);
+	// })
+// $('#btn7').click(function(){
+	// $('#img1').slideUp(2000);
+	// })
+// $('#btn8').click(function(){
+	// $('#img1').slideDown(2000);
+	// })
+// $('#btn9').click(function(){
+	// $('#img1').slideToggle(2000);
+	// })
+// $('#btn10').click(function(){
+	// $('#img1').stop();
+	// })
+	
+
+
+// });
+
+
+// $('document').ready(function(){
+	
+// $('#btn11').click(function(){
+	// $('#img1').animate({
+		// left:'150px',
+		// opacity:'1',
+		// height:'400px',
+		// width:'400px'
+		
+	// // },2000)
+// },'slow')
+	// })
+// });
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // $("p").hide("slow", function(){
+      // alert("The paragraph is now hidden");
+    // });
+  // });
+// });
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // $("#p1").css("color", "red")
+      // .slideUp(2000)
+      // .slideDown(2000);
+  // });
+// });
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // alert("Value: " + $("#test").val());
+  // });
+// });
+
+// $(document).ready(function(){
+  // $("#btn1").click(function(){
+    // $("#test1").text("Hello world!");
+  // });
+  // $("#btn2").click(function(){
+    // $("#test2").html("<b>Hello world!</b>");
+  // });
+  // $("#btn3").click(function(){
+    // $("#test3").val("Dolly Duck");
+  // });
+// });
+
+// $(document).ready(function(){
+  // $("#btn1").click(function(){
+    // $("p").prepend("<b>Prepended text</b>. ");
+  // });
+  // $("#btn2").click(function(){
+    // $("ol").prepend("<li>Prepended item</li>");
+  // });
+// });
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // $("#div1").empty();
+  // });
+// });
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // $("#div1").addClass("important blue");
+  // });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // alert("Background color = " + $("p").css("background-color"));
+  // });
+// });
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // var txt = "";
+    // txt += "Width of div: " + $("#div1").width() + "</br>";
+    // txt += "Height of div: " + $("#div1").height();
+    // $("#div1").html(txt);
+  // });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("span").parent().css({"color": "blue", "border": "2px solid red"});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("div").children("p.first").css({"color": "red", "border": "2px solid red"});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("h2").siblings("p").css({"color": "red", "border": "2px solid red"});
+// });
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("div").last().css("background-color", "yellow");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+  // $("button").click(function(){
+    // $.get("demo_test.asp", function(data, status){
+      // alert("Data: " + data + "\nStatus: " + status);
+    // });
+  // });
+// });
+// ----------------------------------------
+
